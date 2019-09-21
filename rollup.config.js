7c6a79410ddac883ea94823640c6fedf300db28c
Kyle Smith <kysmith@kysmith-mn3.linkedin.biz>
2019-09-20 21:08:29 -0700

    Add lazy loading support and a dev command to watch changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,20 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
 	input: 'src/index.js',
 	output: {
-		file: 'dist/index.js',
-		format: 'iife',
+    dir: 'dist/',
+		format: 'esm',
 	},
 	plugins: [
 		resolve(),
-		babel(),
+    babel(),
+    postcss({ inject: false }),
+		production && terser()
 	]
 }
